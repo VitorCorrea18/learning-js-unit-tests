c608b5faa2b3f6f57b2ed9437ca3654abe8b0f42
Vitor Correa <vitorcorrea18@gmail.com>
2021-11-17 21:29:00 -0400

    requisito 5 completo

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,15 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
+const createStudent = (nome) => {
+  const student = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
 
-const createStudent = () => {};
+  return student;
+};
+
+  console.log(createStudent('Leandrão, o Lobo Solitário'));
 
 module.exports = createStudent;
