b7e9e494c7da3dcbfeceba30471c05c62cc7248f
Vitor Correa <vitorcorrea18@gmail.com>
2021-11-18 12:23:44 -0400

    correcoes lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-let */
-/* eslint-disable block-scoped-let */
-/* eslint-disable no-redeclare */
-/* eslint-disable lets-on-top */
+// /* eslint-disable no-let */
+// /* eslint-disable block-scoped-let */
+// /* eslint-disable no-redeclare */
+// /* eslint-disable lets-on-top */
 
 /*
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
@@ -20,13 +20,11 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
-
-console.log(myCounter());
 
 module.exports = myCounter;
